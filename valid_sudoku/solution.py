28fc497f969ef9c2d771707c3db0785fc26faf68
Song Yu <song0180@e.ntu.edu.sg>
2026-02-19 11:12:26 +0800

    Refactor Sudoku validation logic: Introduced a verbose solution class with detailed checks for rows, columns, and grids, while updating the existing solution's time and space complexity comments.

--- valid_sudoku/solution.py
+++ valid_sudoku/solution.py
@@ -1,6 +1,67 @@
-class Solution:
-    # Time: O(?)
-    # Space: O(?)
+from ast import Set
+from collections import defaultdict
+
+
+class SolutionVerbose:
+    # Time: O(n^2)
+    # Space: O(n)
     def is_valid_sudoku(self, board: list[list[str]]) -> bool:
-        # TODO: Implement is_valid_sudoku
-        return False
+        col_sets: list[Set] = [set() for _ in range(9)]
+        grid_sets: list[list[Set]] = [[set() for _ in range(3)] for _ in range(3)]
+
+        for row_idx, row in enumerate(board):
+            row_set = set()
+            for col_idx, col in enumerate(row):
+                if board[row_idx][col_idx] == ".":
+                    continue
+
+                # check col
+                if col not in col_sets[col_idx]:
+                    col_sets[col_idx].add(col)
+                else:
+                    return False
+
+                # check row
+                if col not in row_set:
+                    row_set.add(col)
+                else:
+                    return False
+
+                # check grid
+                grid_idx_row = row_idx // 3
+                grid_idx_col = col_idx // 3
+
+                if col not in grid_sets[grid_idx_row][grid_idx_col]:
+                    grid_sets[grid_idx_row][grid_idx_col].add(col)
+                else:
+                    return False
+        return True
+
+
+class Solution:
+    # Time: O(n^2)
+    # Space: O(n)
+    def is_valid_sudoku(self, board: list[list[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        grids = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                cur = board[row][col]
+
+                if cur == ".":
+                    continue
+
+                if (
+                    cur in cols[col]
+                    or cur in rows[row]
+                    or cur in grids[(row // 3, col // 3)]
+                ):
+                    return False
+
+                cols[col].add(cur)
+                rows[row].add(cur)
+                grids[(row // 3, col // 3)].add(cur)
+
+        return True
