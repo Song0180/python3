77a189a1b35bbb31c5142c49f0be78be506237f1
Song Yu <song0180@e.ntu.edu.sg>
2026-02-19 12:28:29 +0800

    Add ipykernel and additional dependencies; update playground notebook with execution counts and outputs; implement group_anagrams method using defaultdict for improved performance.

--- group_anagrams/solution.py
+++ group_anagrams/solution.py
@@ -1,6 +1,34 @@
+from collections import defaultdict
+
+
+class SolutionSort:
+    # Time: O(m * nlogn)
+    # Space: O(m * n)
+    def group_anagrams(self, strs: list[str]) -> list[list[str]]:
+        ans = {}
+        for str in strs:
+            key = "".join(sorted(str))
+            if key in ans:
+                ans[key].append(str)
+            else:
+                ans[key] = [str]
+        return ans.values()
+
+
 class Solution:
     # Time: O(?)
-    # Space: O(?)
+    # Space: O(m * n)
     def group_anagrams(self, strs: list[str]) -> list[list[str]]:
-        # TODO: Implement group_anagrams
-        return []
+        ans = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for char in s:
+                count[ord(char) - ord("a")] += 1
+
+            print(tuple(count))
+
+            ans[tuple(count)].append(s)
+
+        return list(ans.values())
