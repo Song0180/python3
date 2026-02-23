d3133c7a550429b6ed446cf3d62deb2accd3bb47
Song Yu <song0180@e.ntu.edu.sg>
2026-02-23 22:51:25 +0800

    Implement combination_sum and combination_sum_dp methods: Add recursive and dynamic programming solutions for finding all unique combinations of candidates that sum to a target, with appropriate return types and logic.

--- combination_sum/solution.py
+++ combination_sum/solution.py
@@ -1,6 +1,33 @@
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
     def combination_sum(self, candidates: list[int], target: int) -> list[list[int]]:
-        # TODO: Implement combination_sum
-        return []
+        ans = []
+        sub_ans = []
+
+        def dfs(i: int, target: int):
+            if target < 0 or i == len(candidates):
+                return
+            if target == 0:
+                ans.append(sub_ans.copy())
+                return
+
+            sub_ans.append(candidates[i])
+            dfs(i, target - candidates[i])
+            sub_ans.pop()
+            dfs(i + 1, target)
+
+        dfs(0, target)
+
+        return ans
+
+    def combination_sum_dp(self, candidates: list[int], target: int) -> list[list[int]]:
+        dp = [[] for _ in range(target + 1)]
+
+        for num in candidates:
+            for i in range(num, target + 1):
+                if i == num:
+                    dp[i].append([num])
+
+                for comb in dp[i - num]:
+                    dp[i].append(comb + [num])
+
+        return dp[target]
