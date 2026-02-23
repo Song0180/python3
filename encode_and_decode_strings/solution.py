a542f8f86b501ad30ba93c6c5187c01940f7e351
Song Yu <song0180@e.ntu.edu.sg>
2026-02-23 17:21:08 +0800

    Implement encoding and decoding methods in Solution class; update playground notebook with example test case and adjust metadata for Python version and display name.

--- encode_and_decode_strings/solution.py
+++ encode_and_decode_strings/solution.py
@@ -1,12 +1,28 @@
 class Solution:
     # Time: O(?)
     # Space: O(?)
-    def encode(self, strs: list[str]) -> str:
-        # TODO: Implement encode
-        return ""
+    """
+    hello, hi, 3y#ou
 
-    # Time: O(?)
-    # Space: O(?)
+    5hello#2hi#33y#ou
+    """
+
+    def encode(self, strs: list[str]) -> str:
+        encoded = ""
+
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+
+        return encoded
+
     def decode(self, s: str) -> list[str]:
-        # TODO: Implement decode
-        return []
+        decoded = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            decoded.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return decoded
