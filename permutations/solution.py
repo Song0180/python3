21ea87485c1860b805239b5f08f461283803d33d
Song Yu <song0180@e.ntu.edu.sg>
2026-02-24 23:18:28 +0800

    Implement permute method: Add depth-first search algorithm to generate all permutations of a list, including detailed time and space complexity comments.

--- permutations/solution.py
+++ permutations/solution.py
@@ -1,6 +1,30 @@
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n!): n is the number of elements in the nums list
+    # Space: O(n): the maximum depth of the recursion tree is n
     def permute(self, nums: list[int]) -> list[list[int]]:
-        # TODO: Implement permute
-        return []
+        ans = []
+        sub_ans = []
+
+        # Depth-first search to generate all permutations
+        # remained is the set of elements that are not yet in the permutation
+        # sub_ans is the current permutation being generated
+        # time complexity is O(n!)
+        # space complexity is O(n)
+        def dfs(remained):
+            if len(remained) == 0:
+                ans.append(sub_ans.copy())
+                return
+
+            for num in remained:
+                sub_ans.append(num)
+                # Create a new set of elements that are not yet in the permutation
+                new_remained = remained.copy()
+                new_remained.remove(num)
+                # Recurse with the new set of elements
+                dfs(new_remained)
+                # Remove the current element from the permutation
+                sub_ans.pop()
+
+        dfs(set(nums))
+
+        return ans
