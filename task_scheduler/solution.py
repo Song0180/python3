3bfbf162f31f5d860092f8d6677662c32f808ebe
Song Yu <song0180@e.ntu.edu.sg>
2026-02-28 18:17:31 +0800

    Implement least_interval method: Add logic to calculate the least number of intervals required to complete all tasks with a cooling period, utilizing a priority queue and deque for efficient task scheduling.

--- task_scheduler/solution.py
+++ task_scheduler/solution.py
@@ -1,6 +1,27 @@
+from collections import Counter, deque
+import heapq
+
+
 class Solution:
     # Time: O(?)
     # Space: O(?)
     def least_interval(self, tasks: list[str], n: int) -> int:
-        # TODO: Implement least_interval
-        return 0
+        cycle = 0
+        freq = Counter(tasks)
+
+        heap = [-f for f in freq.values()]
+        heapq.heapify(heap)
+        q = deque()
+
+        while heap or q:
+            cycle += 1
+
+            if heap:
+                f = heapq.heappop(heap)
+                if f + 1 != 0:
+                    q.append((f + 1, cycle + n))
+
+            if q and q[0][1] == cycle:
+                heapq.heappush(heap, q.popleft()[0])
+
+        return cycle
