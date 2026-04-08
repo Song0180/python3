8f66abc04c61927cae314c1ad36b0a4376aecd52
Song Yu <song0180@e.ntu.edu.sg>
2026-04-08 18:14:15 +0800

    Add k_closest method: Implement logic to find the k closest points to the origin using a max heap, including detailed comments on the approach and complexity considerations.

--- k_closest_points_to_origin/solution.py
+++ k_closest_points_to_origin/solution.py
@@ -1,6 +1,17 @@
+import heapq
+
+
 class Solution:
     # Time: O(?)
     # Space: O(?)
     def k_closest(self, points: list[list[int]], k: int) -> list[list[int]]:
-        # TODO: Implement k_closest
-        return []
+        max_h = []
+
+        for point in points:
+            dist = -(point[0] ** 2 + point[1] ** 2)
+
+            heapq.heappush(max_h, (dist, point))
+            if len(max_h) > k:
+                heapq.heappop(max_h)
+
+        return [p[1] for p in max_h]
