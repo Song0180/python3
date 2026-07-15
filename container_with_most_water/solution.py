5b6e8db94f110044d8a06abf640c68cec14004b1
Song Yu <song0180@e.ntu.edu.sg>
2026-07-15 13:50:54 +0800

    Implement max_area method: Add logic to calculate the maximum area of water that can be contained between two lines, with time complexity of O(n) and space complexity of O(1), enhancing the solution's efficiency and clarity.

--- container_with_most_water/solution.py
+++ container_with_most_water/solution.py
@@ -1,6 +1,18 @@
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(1)
     def max_area(self, height: list[int]) -> int:
-        # TODO: Implement max_area
-        return 0
+        max_area = 0
+
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            max_area = max(max_area, area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return max_area
