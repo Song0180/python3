395c08de8986c01ab187330796ed4ab819a1f10b
Song Yu <song0180@e.ntu.edu.sg>
2026-07-15 14:45:45 +0800

    Implement trap_prefix_suffix_arr method: Add logic to calculate trapped rainwater using prefix and suffix arrays with O(n) time and space complexity, and enhance the existing trap method with a two-pointer approach, improving overall efficiency and clarity.

--- trapping_rain_water/solution.py
+++ trapping_rain_water/solution.py
@@ -1,6 +1,53 @@
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(n)
+    def trap_prefix_suffix_arr(self, height: list[int]) -> int:
+        n = len(height)
+        if n == 0:
+            return 0
+
+        left_max = [0] * n
+        right_max = [0] * n
+
+        # first pass, fill left max. at i, the value is max height on the left (including i itself)
+        left_max[0] = height[0]
+        for i in range(1, n):
+            # why compare with i - 1? left_max[i - 1] represents the max height seen at the left already
+            left_max[i] = max(left_max[i - 1], height[i])
+
+        # second pass, fill right max.
+        right_max[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], height[i])
+
+        res = 0
+
+        for i in range(n):
+            res += min(left_max[i], right_max[i]) - height[i]
+
+        return res
+
+    # 2 pointers
+    # Time: O(n)
+    # Space: O(1)
     def trap(self, height: list[int]) -> int:
-        # TODO: Implement trap
-        return 0
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        left_max, right_max = height[l], height[r]
+        res = 0
+        while l < r:
+            # bounded by left side, so compare left side with current position
+            if left_max < right_max:
+                l += 1
+                # update left max
+                left_max = max(left_max, height[l])
+                res += left_max - height[l]
+            else:
+                r -= 1
+                # update right max
+                right_max = max(right_max, height[r])
+                res += right_max - height[r]
+
+        return res
