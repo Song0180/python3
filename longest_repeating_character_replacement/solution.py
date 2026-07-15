467a4726b325540f61457e926b9c8286189925bb
Song Yu <song0180@e.ntu.edu.sg>
2026-07-15 22:13:55 +0800

    Implement character_replacement method: Add logic to find the length of the longest substring that can be formed by replacing at most k characters, utilizing a sliding window approach with O(n) time complexity and O(m) space complexity, enhancing the solution's efficiency and clarity.

--- longest_repeating_character_replacement/solution.py
+++ longest_repeating_character_replacement/solution.py
@@ -1,6 +1,24 @@
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(m) ~ O(1) as only 26 uppercase chars
     def character_replacement(self, s: str, k: int) -> int:
-        # TODO: Implement character_replacement
-        return 0
+        count = {}
+        l = 0
+        res = 0
+        max_freq = 0
+
+        # grow the window linearly from the right
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            max_freq = max(max_freq, count[s[r]])
+
+            # sub_len - max_freq > k means k cannot cover the char replacement, illegal.
+            # shrink the window from the left until it’s legal again.
+            # we can only shrink from the left to discard some chars until k can cover.
+            while r - l + 1 - max_freq > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res
