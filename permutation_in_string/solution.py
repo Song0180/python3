c3c5308c4cab9307a39d7ad1cc9a03e09c818cf7
Song Yu <song0180@e.ntu.edu.sg>
2026-07-17 11:28:22 +0800

    Implement check_inclusion method: Add logic to determine if one string's permutation exists as a substring in another string using a sliding window approach, achieving O(n) time complexity and O(1) space complexity, enhancing the solution's efficiency and clarity.

--- permutation_in_string/solution.py
+++ permutation_in_string/solution.py
@@ -1,6 +1,34 @@
+from typing import Counter
+
+
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n) (2n -> n)
+    # Space: O(1) (26 eng chars -> 1)
     def check_inclusion(self, s1: str, s2: str) -> bool:
-        # TODO: Implement check_inclusion
+        target_count = Counter(s1)
+        l = 0
+        cur_count = {}
+
+        for r in range(len(s2)):
+            # grow window
+            if s2[r] in target_count:
+                cur_count[s2[r]] = 1 + cur_count.get(s2[r], 0)
+
+                # shrink the left until count is valid
+                while cur_count[s2[r]] > target_count[s2[r]]:
+                    left_char = s2[l]
+                    cur_count[left_char] -= 1
+                    if cur_count[left_char] == 0:
+                        del cur_count[left_char]
+                    l += 1
+
+            # shrink window from the left.
+            else:
+                # a new char not in target appeared, skip all previous window
+                l = r + 1
+                cur_count.clear()
+
+            if cur_count == target_count:
+                return True
+
         return False
