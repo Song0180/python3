a49d4b146c8f860050fddd3bb06f48933a9444ae
Song Yu <song0180@e.ntu.edu.sg>
2026-07-18 16:21:05 +0800

    Refactor binary tree view methods: Rename right_side_view to right_side_view_bfs and implement a new right_side_view method using depth-first search (DFS). Update space complexity comments for clarity, enhancing the solution's efficiency and readability.

--- binary_tree_right_side_view/solution.py
+++ binary_tree_right_side_view/solution.py
@@ -4,8 +4,8 @@ from collections import deque
 
 class Solution:
     # Time: O(n): n is the number of nodes in the tree
-    # Space: O(d): d is the depth of the tree
-    def right_side_view(self, root: TreeNode[int] | None) -> list[int]:
+    # Space: O(w + h): w: max width of the tree,  h: tree height. worst total: O(n)
+    def right_side_view_bfs(self, root: TreeNode[int] | None) -> list[int]:
         ans = []
         q = deque([root])
 
@@ -25,3 +25,23 @@ class Solution:
                 ans.append(level[-1])
 
         return ans
+
+    # dfs
+    # time: O(n)
+    # space: O(h)
+    def right_side_view(self, root: TreeNode[int] | None) -> list[int]:
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return
+
+            # this is the first node at this depth → append its value.
+            if len(res) == depth:
+                res.append(node.val)
+
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+
+        dfs(root, 0)
+        return res
--- validate_binary_search_tree/solution.py
+++ validate_binary_search_tree/solution.py
@@ -1,10 +1,11 @@
+from collections import deque
 from leetcode_py import TreeNode
 
 
 class Solution:
     # Time: O(n): n is the number of nodes in the tree
     # Space: O(h): h is the height of the tree
-    def is_valid_bst(self, root: TreeNode[int] | None) -> bool:
+    def is_valid_bst_dfs(self, root: TreeNode[int] | None) -> bool:
         def dfs(root, min_val, max_val):
             if not root:
                 return True
@@ -17,3 +18,24 @@ class Solution:
             )
 
         return dfs(root, float("-inf"), float("inf"))
+
+    # bfs
+    # time: O(n)
+    # space: O(w) q length -> worst O(n) balanced tree, n/2; best: O(1), skewed tree
+    def is_valid_bst(self, root: TreeNode[int] | None) -> bool:
+        if not root:
+            return True
+
+        q = deque([(root, float("-inf"), float("inf"))])
+        while q:
+            node, lower, upper = q.popleft()
+
+            if node.val <= lower or node.val >= upper:
+                return False
+
+            if node.left:
+                q.append((node.left, lower, node.val))
+            if node.right:
+                q.append((node.right, node.val, upper))
+
+        return True
