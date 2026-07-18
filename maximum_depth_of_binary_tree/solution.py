e90f35d0f0dbb735b4561d0ffca2029c68a863d7
Song Yu <song0180@e.ntu.edu.sg>
2026-07-18 14:31:55 +0800

    Implement max_depth methods for binary tree: Add depth-first search (DFS) and breadth-first search (BFS) approaches to calculate the maximum depth of a binary tree, achieving O(n) time complexity and O(height) space complexity, enhancing the solution's efficiency and clarity.

--- maximum_depth_of_binary_tree/solution.py
+++ maximum_depth_of_binary_tree/solution.py
@@ -1,9 +1,36 @@
+from collections import deque
 from leetcode_py import TreeNode
 
 
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(height), best/balanced: O(logn), worst/skewed: O(n)
+    def max_depth_dfs(self, root: TreeNode[int] | None) -> int:
+        if not root:
+            return 0
+
+        left_depth = self.max_depth(root.left)
+        right_depth = self.max_depth(root.right)
+
+        return 1 + max(left_depth, right_depth)
+
+    # bfs
     def max_depth(self, root: TreeNode[int] | None) -> int:
-        # TODO: Implement max_depth
-        return 0
+        if not root:
+            return 0
+
+        depth = 0
+
+        q = deque([root])
+        while q:
+            level_len = len(q)
+            depth += 1
+            # only pop out nodes from current level and add next level
+            for _ in range(level_len):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return depth
