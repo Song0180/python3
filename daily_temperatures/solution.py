ca39cd704cfcda1797cc4136021cc1708107d7cb
Song Yu <song0180@e.ntu.edu.sg>
2026-07-18 10:41:17 +0800

    Implement daily_temperatures method: Add logic to calculate the number of days until a warmer temperature for each day using a monotonic stack approach, achieving O(n) time complexity and O(n) space complexity, enhancing the solution's efficiency and clarity.

--- daily_temperatures/solution.py
+++ daily_temperatures/solution.py
@@ -2,5 +2,17 @@ class Solution:
     # Time: O(?)
     # Space: O(?)
     def daily_temperatures(self, temperatures: list[int]) -> list[int]:
-        # TODO: Implement daily_temperatures
-        return []
+        res = [0] * len(temperatures)
+        # monotonic decreasing stack, as only same or smaller new values can be inserted
+        stack = []
+
+        for i, t in enumerate(temperatures):
+
+            # found a temp higher than prev temps in stack
+            while stack and t > stack[-1][0]:
+                _, idx = stack.pop()
+                res[idx] = i - idx
+
+            # add current temp, idx in stack
+            stack.append((t, i))
+        return res
--- invert_binary_tree/solution.py
+++ invert_binary_tree/solution.py
@@ -1,9 +1,40 @@
+from collections import deque
 from leetcode_py import TreeNode
 
 
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(h)
+    # dfs
+    def invert_tree_dfs(self, root: TreeNode[int] | None) -> TreeNode[int] | None:
+        if not root:
+            return None
+
+        left_node = self.invert_tree(root.left)
+        right_node = self.invert_tree(root.right)
+
+        root.left = right_node
+        root.right = left_node
+
+        return root
+
+    # bfs
+    # t: O(n)
+    # s: O(n)
     def invert_tree(self, root: TreeNode[int] | None) -> TreeNode[int] | None:
-        # TODO: Implement invert_tree
-        return None
+        if not root:
+            return None
+
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                q.append(node.left)
+
+            if node.right:
+                q.append(node.right)
+
+        return root
