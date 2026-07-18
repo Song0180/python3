6e762b33306e49ae2478fabafbfeebcce9325380
Song Yu <song0180@e.ntu.edu.sg>
2026-07-18 15:01:56 +0800

    Implement diameter_of_binary_tree method: Add logic to calculate the diameter of a binary tree using depth-first search (DFS), achieving O(n) time complexity and O(h) space complexity, enhancing the solution's efficiency and clarity.

--- diameter_of_binary_tree/solution.py
+++ diameter_of_binary_tree/solution.py
@@ -2,8 +2,24 @@ from leetcode_py import TreeNode
 
 
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(h): O(logn) ~ O(n)
     def diameter_of_binary_tree(self, root: TreeNode[int] | None) -> int:
-        # TODO: Implement diameter_of_binary_tree
-        return 0
+        max_dia = 0
+
+        def dfs(node):
+            nonlocal max_dia
+
+            if not node:
+                return 0
+
+            left_d = dfs(node.left)
+            right_d = dfs(node.right)
+
+            # at any given node, diameter = left depth + right depth
+            max_dia = max(max_dia, left_d + right_d)
+
+            return 1 + max(left_d, right_d)
+
+        dfs(root)
+        return max_dia
