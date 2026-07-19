ee507017b080b6527871c94adb08c6f3b704f852
Song Yu <song0180@e.ntu.edu.sg>
2026-07-19 15:28:21 +0800

    Implement remove_nth_from_end methods for linked list: Add two approaches to remove the nth node from the end, one using a two-pass technique and the other utilizing a single-pass with two pointers, both achieving O(n) time complexity and O(1) space complexity, enhancing the solution's efficiency and clarity.

--- remove_nth_node_from_end_of_list/solution.py
+++ remove_nth_node_from_end_of_list/solution.py
@@ -2,10 +2,52 @@ from leetcode_py import ListNode
 
 
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(1)
+    def remove_nth_from_end_two_pass(
+        self, head: ListNode[int] | None, n: int
+    ) -> ListNode[int] | None:
+        length = 0
+        cur = head
+        while cur:
+            length += 1
+            cur = cur.next
+
+        idx = length - n
+
+        if idx == 0:
+            return head.next
+
+        prev = ListNode(0, head)
+        cur = head
+        step = 0
+
+        while step < idx:
+            step += 1
+            prev = prev.next
+            cur = cur.next
+
+        prev.next = cur.next
+
+        return head
+
+    # time: O(n)
+    # space: O(1)
+    # single pass, 2 pointers
     def remove_nth_from_end(
         self, head: ListNode[int] | None, n: int
     ) -> ListNode[int] | None:
-        # TODO: Implement remove_nth_from_end
-        return head
+        dummy = ListNode(0, head)
+        first = dummy
+        second = head
+
+        for _ in range(n):
+            second = second.next
+
+        while second:
+            second = second.next
+            first = first.next
+
+        first.next = first.next.next
+
+        return dummy.next
