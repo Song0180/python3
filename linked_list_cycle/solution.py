b8a0d04dfe5b17071fe969444fa4be1045ea19cb
Song Yu <song0180@e.ntu.edu.sg>
2026-07-19 14:26:21 +0800

    Implement has_cycle method for linked list: Add logic to detect a cycle using the Floyd's Tortoise and Hare algorithm, achieving O(n) time complexity and O(1) space complexity, enhancing the solution's efficiency and clarity.

--- linked_list_cycle/solution.py
+++ linked_list_cycle/solution.py
@@ -5,5 +5,14 @@ class Solution:
     # Time: O(?)
     # Space: O(?)
     def has_cycle(self, head: ListNode[int] | None) -> bool:
-        # TODO: Implement has_cycle
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            # do not use "==" for comparison
+            if fast is slow:
+                return True
+
         return False
