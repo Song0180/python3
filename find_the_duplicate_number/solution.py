55d25c9177b0ebae10a18779d764380d8a268dec
Song Yu <song0180@e.ntu.edu.sg>
2026-07-19 18:55:34 +0800

    Implement find_duplicate method: Add logic to identify the duplicate number in a list by marking visited numbers as negative, achieving O(n) time complexity and O(1) space complexity, enhancing the solution's efficiency and clarity.

--- find_the_duplicate_number/solution.py
+++ find_the_duplicate_number/solution.py
@@ -2,5 +2,12 @@ class Solution:
     # Time: O(?)
     # Space: O(?)
     def find_duplicate(self, nums: list[int]) -> int:
-        # TODO: Implement find_duplicate
-        return nums[0]
+        for i in range(len(nums)):
+            # number may be marked negative, to calculate idx must use abs
+            idx = abs(nums[i]) - 1
+            if nums[idx] < 0:
+                # number may be marked negative already
+                return abs(nums[i])
+
+            # mark number as visited
+            nums[idx] *= -1
