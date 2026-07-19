9a8932f04e7c37e07c02ba8f06867022ded0017c
Song Yu <song0180@e.ntu.edu.sg>
2026-07-19 14:52:09 +0800

    Implement reorder_list method: Add logic to reorder a linked list in-place by finding the midpoint, reversing the second half, and merging both halves, achieving O(n) time complexity and O(1) space complexity, enhancing the solution's efficiency and clarity.

--- reorder_list/solution.py
+++ reorder_list/solution.py
@@ -2,8 +2,37 @@ from leetcode_py import ListNode
 
 
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(n)
+    # Space: O(1)
     def reorder_list(self, head: ListNode[int] | None) -> None:
-        # TODO: Implement reorder_list
-        pass
+        slow = fast = head
+
+        # 1: find mid point
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2: reverse second half
+        second, cur = None, slow.next
+        # don't for get to cut first half
+        slow.next = None
+
+        while cur:
+            temp = cur.next
+            cur.next = second
+            second = cur
+            cur = temp
+
+        # 3: merge first and second
+        first = head
+
+        # second is equal or shorter, we can stop after second list is traversed
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            # alternate
+            first.next = second
+            second.next = tmp1
+
+            # progress
+            first = tmp1
+            second = tmp2
