03bf17329aaa067bcb0233beae714e95f65fa441
Song Yu <song0180@e.ntu.edu.sg>
2026-07-19 13:12:21 +0800

    Implement MedianFinder class: Add logic to maintain two heaps for efficient median calculation, achieving O(log(n)) time complexity for adding numbers and O(1) for finding the median, enhancing the solution's efficiency and clarity.

--- find_median_from_data_stream/solution.py
+++ find_median_from_data_stream/solution.py
@@ -1,18 +1,37 @@
+import heapq
+
+
 class MedianFinder:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(1)
+    # Space: O(n)
     def __init__(self) -> None:
-        # TODO: Initialize
-        pass
+        self.min_heap = []
+        self.max_heap = []
 
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(log(n))
+    # Space: O(1)
     def add_num(self, num: int) -> None:
-        # TODO: Implement add_num
-        pass
+        if self.max_heap and num <= -self.max_heap[0]:
+            heapq.heappush(self.max_heap, -num)
+        else:
+            heapq.heappush(self.min_heap, num)
 
-    # Time: O(?)
-    # Space: O(?)
+        # rebalance
+        if len(self.max_heap) < len(self.min_heap):
+            while len(self.min_heap) - len(self.max_heap) > 1:
+                val = heapq.heappop(self.min_heap)
+                heapq.heappush(self.max_heap, -val)
+        else:
+            while len(self.max_heap) - len(self.min_heap) > 1:
+                val = heapq.heappop(self.max_heap)
+                heapq.heappush(self.min_heap, -val)
+
+    # Time: O(1)
+    # Space: O(1)
     def find_median(self) -> float:
-        # TODO: Implement find_median
-        return 0.0
+        if len(self.min_heap) == len(self.max_heap):
+            return (self.min_heap[0] - self.max_heap[0]) / 2
+        elif len(self.min_heap) < len(self.max_heap):
+            return -self.max_heap[0]
+        else:
+            return self.min_heap[0]
