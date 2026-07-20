4b7282dd2499d59c210542eee3d7e83b1a9a8942
Song Yu <song0180@e.ntu.edu.sg>
2026-07-20 09:49:53 +0800

    Implement clone_graph method: Add depth-first search and breadth-first search implementations to clone a graph, including time and space complexity annotations for improved clarity and understanding of the algorithms' efficiency.

--- clone_graph/solution.py
+++ clone_graph/solution.py
@@ -1,9 +1,46 @@
+from collections import deque
 from leetcode_py import GraphNode
 
 
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
+    # Time: O(V + E)
+    # Space: O(V)
+    def clone_graph_dfs(self, node: GraphNode | None) -> GraphNode | None:
+        if not node:
+            return None
+
+        old_new_map: dict[int, GraphNode] = {}
+
+        def dfs(cur: GraphNode) -> GraphNode:
+            node_id = id(cur)
+            if node_id in old_new_map:
+                return old_new_map[node_id]
+
+            copy = GraphNode(cur.val)
+            old_new_map[node_id] = copy
+
+            for nei in cur.neighbors:
+                nei_node = dfs(nei)
+                copy.neighbors.append(nei_node)
+
+            return copy
+
+        return dfs(node)
+
     def clone_graph(self, node: GraphNode | None) -> GraphNode | None:
-        # TODO: Implement clone_graph
-        return None
+        if not node:
+            return None
+
+        old_new_map: dict[int, GraphNode] = {id(node): GraphNode(node.val)}
+        q = deque([node])
+
+        while q:
+            cur = q.popleft()
+
+            for nei in cur.neighbors:
+                if id(nei) not in old_new_map:
+                    old_new_map[id(nei)] = GraphNode(nei.val)
+                    q.append(nei)
+                old_new_map[id(cur)].neighbors.append(old_new_map[id(nei)])
+
+        return old_new_map[id(node)]
