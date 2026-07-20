e0dd5f8888feb2dd66feda4bc98f44c1ba807a28
Song Yu <song0180@e.ntu.edu.sg>
2026-07-20 13:09:46 +0800

    Implement count_components method: Add depth-first search and breadth-first search implementations to count the number of connected components in an undirected graph, including time and space complexity annotations for improved clarity and understanding of the algorithms' efficiency.

--- number_of_connected_components_in_an_undirected_graph/solution.py
+++ number_of_connected_components_in_an_undirected_graph/solution.py
@@ -1,6 +1,59 @@
+from collections import deque
+
+
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
-    def count_components(self, n: int, edges: list[list[int]]) -> int:
-        # TODO: Implement count_components
-        return 0
+    # Time: O(V + E)
+    # Space: O(V + E)
+    def count_components_dfs(self, n: int, edges: list[list[int]]) -> int:
+        adj_map = {i: [] for i in range(n)}
+
+        for a, b in edges:
+            # undirected graph, need to update both nodes
+            adj_map[a].append(b)
+            adj_map[b].append(a)
+
+        visited = set()
+
+        def dfs(node):
+            visited.add(node)
+
+            for nei in adj_map[node]:
+                if nei not in visited:
+                    dfs(nei)
+
+        count = 0
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                count += 1
+
+        return count
+
+    # dfs, O(V + E)
+    def count_components(self, n, edges):
+        adj_map = {i: [] for i in range(n)}
+
+        for a, b in edges:
+            # undirected graph, need to update both nodes
+            adj_map[a].append(b)
+            adj_map[b].append(a)
+
+        visited = set()
+
+        def bfs(node):
+            q = deque([node])
+
+            while q:
+                n = q.popleft()
+                visited.add(n)
+                for nei in adj_map[n]:
+                    if nei not in visited:
+                        q.append(nei)
+
+        count = 0
+        for i in range(n):
+            if i not in visited:
+                bfs(i)
+                count += 1
+
+        return count
