6be6df5d4d4940692f058b585b35e1e0d0564feb
Song Yu <song0180@e.ntu.edu.sg>
2026-07-20 11:52:23 +0800

    Implement course scheduling methods: Add depth-first search implementations for can_finish and find_order methods to determine if all courses can be completed and to find the order of courses, respectively. Include time and space complexity annotations for improved clarity and understanding of the algorithms' efficiency.

--- course_schedule/solution.py
+++ course_schedule/solution.py
@@ -1,6 +1,34 @@
 class Solution:
-    # Time: O(?)
-    # Space: O(?)
-    def can_finish(self, num_courses: int, prerequisites: list[list[int]]) -> bool:
-        # TODO: Implement can_finish
-        return False
+    # Time: O(V + E)
+    # Space: O(V + E)
+    def can_finish_dfs(self, num_courses: int, prerequisites: list[list[int]]) -> bool:
+        pre_map = {i: [] for i in range(num_courses)}
+
+        for c, p in prerequisites:
+            pre_map[c].append(p)
+
+        visiting = set()
+        completed = set()
+
+        def dfs(course):
+            if course in visiting:
+                return False
+
+            if course in completed:
+                return True
+
+            visiting.add(course)
+
+            for pre in pre_map[course]:
+                if not dfs(pre):
+                    return False
+
+            visiting.remove(course)
+            completed.add(course)
+            return True
+
+        for i in range(num_courses):
+            if not dfs(i):
+                return False
+
+        return True
--- course_schedule_ii/solution.py
+++ course_schedule_ii/solution.py
@@ -2,5 +2,34 @@ class Solution:
     # Time: O(?)
     # Space: O(?)
     def find_order(self, num_courses: int, prerequisites: list[list[int]]) -> list[int]:
-        # TODO: Implement find_order
-        return []
+        order = []
+
+        pre_map = {i: [] for i in range(num_courses)}
+        for c, p in prerequisites:
+            pre_map[c].append(p)
+
+        visiting = set()
+        complete = set()
+
+        def dfs(course):
+            if course in visiting:
+                return False
+
+            if course in complete:
+                return True
+
+            visiting.add(course)
+            for pre in pre_map.get(course):
+                if not dfs(pre):
+                    return False
+            visiting.remove(course)
+
+            complete.add(course)
+            order.append(course)
+            return True
+
+        for i in range(num_courses):
+            if not dfs(i):
+                return []
+
+        return order
